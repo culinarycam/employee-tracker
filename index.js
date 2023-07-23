527624e44910d727fcbb70b5069d9c049aaadaea
Cameron Singleton <culinarycam98@gmail.com>
2023-07-23 10:39:40 -0400

    added handle action const

--- index.js
+++ index.js
@@ -3,6 +3,38 @@ const mysql = require('mysql2');
 
 const prompt = inquirer.createPromptModule();
 
+let db;
+
+const handleAction = ({ action }) => {
+    console.log(`ACTION ${action}`);
+    switch(action) {
+        case 'View All Employees': {
+          db.query('SELECT * FROM employees', (err, employees) => {
+            console.table(employees);
+            init();
+          });
+            break;
+        }
+        case 'View All Departments': {
+          db.query('SELECT * FROM departments', (err, departments) => {
+            console.table(departments);
+            init();
+          });
+          break;
+        }
+        case 'View All Roles': {
+            db.query('SELECT * FROM roles', (err, roles) => {
+                console.table(roles);
+                init();
+            });
+            break;
+        }
+        default: {
+            process.exit();
+        }
+    }
+};
+
 const init = () => {
     prompt({
         message: 'What would you like to do?',
@@ -16,12 +48,8 @@ const init = () => {
     }).then(handleAction);
 };
 
-const db = mysql.createConnection({
+db = mysql.createConnection({
     host: '3001',
     user: 'root',
     database: 'employee_db',
 }, init());
-
-const handleAction = ({ action }) => {
-    console.log(`ACTION ${action}`);
-}
